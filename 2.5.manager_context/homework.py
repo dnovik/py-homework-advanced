9c96d0174c1050624aa43373fd88e5e2193a6599
Denis Novik <dnovik01@gmail.com>
2019-05-01 16:03:57 +0300

    Домашняя работа с учетом комментариев

--- 2.5.manager_context/homework.py
+++ 2.5.manager_context/homework.py
@@ -2,12 +2,14 @@ from datetime import datetime
 
 class OpenerFile():
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, mode):
         self.file_path = file_path
+        self.mode = mode
         self.start_time = datetime.now()
 
     def __enter__(self):
-        self.file = open(self.file_path)
+        self.file = open(self.file_path, self.mode)
+
 
         return self.file
 
@@ -29,11 +31,18 @@ class OpenerFile():
         pass
 
 
-file = r'test.txt'
 
-with OpenerFile(file) as f:
-    print(f.read())
+import requests
+import json 
 
+link = 'https://raw.githubusercontent.com/netology-code/py-homework-basic-files/master/3.1.formats.json.xml/newsafr.json'
+
+news = requests.get(link).json()
+file = r'D:\Python\Netology\2.5.manager_context\news.json'
+
+with OpenerFile(file, 'w') as f:
+    data_to_write = json.dumps(news)
+    f.write(data_to_write)
 
 
 
