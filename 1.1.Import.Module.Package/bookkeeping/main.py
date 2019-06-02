b7410b0111b9d1b07cc710dcb40e72bf8b533539
Denis Novik <dnovik01@gmail.com>
2019-06-02 15:31:53 +0300

    Структура бухгалтерии

---
+++ 1.1.Import.Module.Package/bookkeeping/main.py
@@ -0,0 +1,11 @@
+from salary import calculate_salary
+from people import get_employees
+
+
+def main():
+    print('Добро пожаловать в программу "Бухгалтерия"!')
+    print(calculate_salary())
+    print(get_employees())
+
+if __name__ == '__main__':
+    main()
