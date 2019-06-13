6b1ade3bea233c919c0565ed8ed398254a9e4e89
Denis Novik <dnovik01@gmail.com>
2019-06-13 19:45:08 +0300

    Resolved

--- 1.3.Decorators/homework.py
+++ 1.3.Decorators/homework.py
@@ -1,11 +1,14 @@
 
 from datetime import datetime
-from homework_files import file, add_to_recipes
+import requests
+
 
 FILE_TO_WRITE = r'logger.txt'
+API_KEY = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 
 
 # ЗАДАНИЕ 1
+
 
 def logger(func):
 
@@ -29,14 +32,13 @@ def raise_to_pow(num):
     result = num ** 2
     return result
 
-
+# выполняем функцию для проверки декоратора
 raise_to_pow(11)
 
 
 # ЗАДАНИЕ 2
 
 def logger(func):
-
 
     def get_func_info(*args):
         func_name = func.__name__
@@ -58,9 +60,36 @@ def raise_to_pow(num):
     result = num ** 2
     return result
 
-
+# выполняем функцию для проверки декоратора
 raise_to_pow(11, FILE_TO_WRITE)
 
+
 # ЗАДАНИЕ 3
+# Функция взята из домашнего задания по теме: 3.2.http.requests
+
 @logger
-add_to_recipes(file)
+def get_lang_id(language_name):
+    # функция принимает название языка на русском и возвращает его параметр в виде id из Яндекс.Переводчик API
+
+    langs_url = 'https://translate.yandex.net/api/v1.5/tr.json/getLangs'
+
+    params = {
+        'key': API_KEY,
+        'ui': 'ru'
+    }
+
+    langs = requests.get(langs_url, params).json()
+
+    lang_dict = langs['langs']
+
+    for lang_id, lang_name in lang_dict.items():
+        if language_name == lang_name:
+            response = lang_id
+            break
+        else:
+            response = 'Язык не найден'
+    return response
+
+
+# выполняем функцию для проверки декоратора
+get_lang_id('Немецкий', FILE_TO_WRITE)
